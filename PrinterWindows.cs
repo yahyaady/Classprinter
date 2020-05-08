fb059fd018cec71a6ea2ccf2f1f25a6a670123d5
yahyaardiyanto <yahyaardi33@gmail.com>
2020-05-08 18:39:46 +0700

    Classprinter

---
+++ PrinterWindows.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classprinter
+{
+    public class PrinterWindows
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine();
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine();
+        }
+    }
+}
